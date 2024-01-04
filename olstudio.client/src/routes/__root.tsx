e47d523a299371ab38f209de7908def963628f90
Toma Rosa <toma.rosa@protonmail.com>
2024-01-04 15:40:30 +0100

    fix(client): full height support

--- olstudio.client/src/routes/__root.tsx
+++ olstudio.client/src/routes/__root.tsx
@@ -4,13 +4,16 @@ import { Outlet, RootRoute } from "@tanstack/react-router";
 export const Route = new RootRoute({
   component: function Root() {
     return (
-      <>
+      <div className="flex h-full flex-col">
         <header className="flex items-center justify-between border-b py-4 px-4 sm:px-6 lg:px-20 xl:px-24">
           root
           <ModeToggle />
         </header>
-        <Outlet />
-      </>
+
+        <main className="flex-1">
+          <Outlet />
+        </main>
+      </div>
     );
   },
 });
